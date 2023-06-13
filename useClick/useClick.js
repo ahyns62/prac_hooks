8b49d7febd4c8fd4c0f3a2c786c98537d4a98e7d
Ahyeon Lee <laah0602@gmail.com>
2023-06-14 01:04:46 +0900

    [ADD] upload files

--- useClick/useClick.js
+++ useClick/useClick.js
@@ -4,6 +4,7 @@ export const useClick = (onClick) => {
   if (typeof onClick !== "function") {
     return;
   }
+
   const element = useRef(); // DOM에 ref 지정을 통한 접근. like getElementById()
   useEffect(() => {
     if (element.current) {
